c5f20a5b794882ac82e5487646e75160b85c60d0
Viraj-Mathur <virajmathur5@gmail.com>
2026-03-28 15:11:58 -0400

    Fourth Commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,6 +8,17 @@ def emotion_detector(text_to_analyze):
     
     response = requests.post(url, headers=headers, json=input_json)
     
+    # Handle blank entry - status code 400
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+    
     # Convert response text to dictionary
     response_dict = json.loads(response.text)
     
